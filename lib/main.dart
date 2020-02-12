7c0634c30fa55b56606d5ea2b88916cf476b24d5
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-02-12 14:25:09 +0100

    UI Successfully made, Card Widget Added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:weather_mate/scrollCard.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-      Widget build(BuildContext context) {
-      return MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
         title: 'Flutter Weather App',
         theme: ThemeData(
-          primaryColorDark: Colors.black
+          primaryColor: Color(0xFFAAAAAA),
         ),
 
         home: Scaffold(
-          backgroundColor: Colors.black45,
-          appBar: AppBar(
-            backgroundColor: Colors.black45,
-          ),
-          body:
-
-          Stack(
+            backgroundColor:Color(0xFF222222),
+            appBar: AppBar(
+              leading: Icon(Icons.menu, color: Colors.white,),
+              actions: <Widget>[Icon(Icons.add, color: Colors.white)],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            body: ListView(padding: EdgeInsets.only(left: 10, right: 10, top: 30),
             children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child:Icon(Icons.dehaze, size: 30, color: Colors.white,) ,
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text('Sector 2244',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
               ),
-              Align(
-                alignment: Alignment.topRight,
-                child:Icon(Icons.add, size: 30, color: Colors.white,),
-              ),
-              Row(
-                Text('Enugu',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )
+              Padding(
+                padding: const EdgeInsets.only(right: 8, left: 8, top: 30, bottom: 40),
+                child: TempCard(),
               )
             ],
-          ),
-          ),
-      );
-}
 
+            )
+        ));
+  }
 }
---
+++ lib/scrollCard.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TempCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '30',
+              style: TextStyle(
+                  fontSize: 90,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white),
+            ),
+            Text(
+              'Clear',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 120, bottom: 70),
+              child: Text(
+                'Light raidndodiojidmipdoojkdmpodlr jo j',
+                style: TextStyle(
+                  color: Colors.white54,
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.white12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  '28 --- 40',
+                  style: TextStyle(color: Colors.white54),
+                ),
+                Icon(
+                  Icons.cloud,
+                  color: Colors.red,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
